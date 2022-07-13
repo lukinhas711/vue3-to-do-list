3fe93ada33ee79cec096de63968ebcfd36f2b25a
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-07-12 22:11:09 -0300

    chore: format

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"; // * inportando o createApp de dentro do pacote Vue
+import App from "./App.vue"; // * salvando o arquivo app.vue dentro da variavel Vue
 
-createApp(App).mount('#app')
+createApp(App).mount("#app"); // * Usando o createApp para montar o componente no meu id app que esta dentro do index.html na pasta public
